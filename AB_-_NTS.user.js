6d61e173a264b25996cf0b2f618d71feafda6581
p3scripts <p3scripts@users.noreply.github.com>
2017-03-02 21:26:58 +0100

    AB - Not Tiny Searchbox

---
+++ AB_-_NTS.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        AB - Not Tiny Searchbox
+// @author 		psyntax3rr0r
+// @description Not Tiny Searchbox
+// @include     https://animebytes.tv/torrents.php*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$("#series_name_anime").removeClass("series_name standard_fields");
+$("#series_name_anime").outerWidth('300');
