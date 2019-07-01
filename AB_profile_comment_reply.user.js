9c63d3c9076de317a0fd5f37b2e6206969ac2358
p3scripts <p3scripts@users.noreply.github.com>
2019-07-01 10:47:11 +0200

    Update AB_profile_comment_reply.user.js

--- AB_profile_comment_reply.user.js
+++ AB_profile_comment_reply.user.js
@@ -6,12 +6,14 @@
 // @icon        http://animebytes.tv/favicon.ico
 // @include     *https://animebytes.tv/user.php?*
 // @version     1
+// @downloadURL		https://github.com/p3scripts/ab_scripts/raw/master/AB_profile_comment_reply.user.js
+// @updateURL		https://github.com/p3scripts/ab_scripts/raw/master/AB_profile_comment_reply.user.js
 // @grant       none
 // ==/UserScript==
 
 $("a.strong.usercomment_posttime").each( function () {
 	$author = $(this).parent().prev(":first-child").children(":first-child").attr("href");
-	$(this).before('<a href="'+$author+'#quickpost" class="strong">[Reply]</a>');
+	$(this).before('<a href="'+$author+'#quickpost" class="strong">[&#10150;]</a>');
 });
 
 $(document).ready(function () {
