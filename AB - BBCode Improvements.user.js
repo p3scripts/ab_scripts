eb8145581a306b1c5cef2f92fe6469e6230d89dd
p3scripts <p3scripts@users.noreply.github.com>
2019-07-01 11:07:25 +0200

    Added Download / Update URL
    Merged and fixed version of BBCode ColorPicker and BBCode improvements

--- AB - BBCode Improvements.user.js
+++ AB - BBCode Improvements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 			AB - BBCode Improvements
 // @author 			p3
-// @version 		1
+// @version 		1.1
 // @downloadURL		https://github.com/p3scripts/ab_scripts/raw/master/AB%20-%20BBCode%20Improvements.user.js
 // @updateURL		https://github.com/p3scripts/ab_scripts/raw/master/AB%20-%20BBCode%20Improvements.user.js
 // @description 	BBCode Improvements
